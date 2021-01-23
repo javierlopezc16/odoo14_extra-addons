7a1f960724799b9363609bf1ab32482f3f3a4d03
javierlopezc16 <46999460+javierlopezc16@users.noreply.github.com>
2021-01-23 18:26:33 -0300

    Create __manifest__.py

---
+++ account_journal_outstanding_accounts/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Account Journal Outstading Accounts",
+    'summary': "",
+    'description': """""",
+    'author': "Sr. Robot",
+    'license': "AGPL-3",
+    'website': "https://www.bmya.cl",
+    'category': 'Uncategorized',
+    'version': '14.0',
+    'depends': ['account',
+    ],
+    'data': ['views/account_journal.xml',
+    ],
+
+}
